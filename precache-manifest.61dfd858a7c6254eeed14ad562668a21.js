4d54b829d250031b7c458c74504cb2f8d7eabf60
Vad GFK <vadgfk@gmail.com>
2020-10-01 14:35:45 -0400

    Updates

---
+++ precache-manifest.61dfd858a7c6254eeed14ad562668a21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11821f341bfce98b7a8084a2be10d818",
+    "url": "/background-repository/index.html"
+  },
+  {
+    "revision": "0bb8f28e9d08faab7b3f",
+    "url": "/background-repository/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/background-repository/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/background-repository/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb8f28e9d08faab7b3f",
+    "url": "/background-repository/static/js/main.977bee8c.chunk.js"
+  },
+  {
+    "revision": "bd524ca49c00ed75350a",
+    "url": "/background-repository/static/js/runtime-main.a393881a.js"
+  }
+]);
